c1cf10109aebbfbf85e1eb728cf3373a5b3b4dde
Hiperiosity <anastasy_ya@mail.ru>
2024-04-16 19:14:20 +0300

    change cors api

--- app.js
+++ app.js
@@ -39,8 +39,8 @@ app.use(cors({
   origin: [
     'http://localhost:3000',
     'http://localhost:3001',
-    'https://movies.anastkm4.beget.tech',
-    'http://movies.anastkm4.beget.tech',
+    'http://api.anastasy-ya.diplom.nomoredomains.xyz',
+    'http://anastasy-ya.diplom.nomoredomains.xyz',
   ],
   credentials: true,
 }));
